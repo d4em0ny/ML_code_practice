3a8b8f509b1d19389e3cea7e14f88444a817e4bc
Yoseph won <yosephwondimu26@gmail.com>
2026-08-05 14:25:09 +0300

    complete Monte Carlo RL implementation

--- monte-carlo/agent.py
+++ monte-carlo/agent.py
@@ -1,68 +1,52 @@
-import random
+import random as rnd
 
 class MCAgent:
-    epsilon = 0.1
     gamma = 1
+    epsilon = 0.1
 
     def __init__(self):
         self.Q = {}
         self.returns_sum = {}
         self.returns_count = {}
 
+    def calculate_returns(self, episode):
+        G = 0
+        returns = []
+
+        for _, _, R in reversed(episode):
+            G = R + self.gamma * G
+
+            returns.append(G)
+
+        returns.reverse()
+        return returns
+
     def choose_action(self, state):
-        #Explore
-        if random.random() < self.epsilon:
-            return random.choice([0, 1])
+        if rnd.random() < self.epsilon:
+            return rnd.choice([0, 1])
 
         q_values = [
             self.Q.get((state, 0), 0),
             self.Q.get((state, 1), 0),
         ]
 
-        max_q = max(q_values)
+        max_val = max(q_values)
 
-        best_actions = [
-            action
-            for action, q in enumerate(q_values)
-            if q == max_q
-        ]
+        best_action = [action for action, value in enumerate(q_values) if value == max_val]
 
-        return random.choice(best_actions)
+        return rnd.choice(best_action)
 
-    """
-        for each (state, action, reward). 
-        calculate their reward starting from the back
-    """
-    def calculate_returns(self, episode):
-        returns = []
-        G = 0
-
-        for _, _, reward in reversed(episode):
-            G = reward + self.gamma * G
-            returns.append(G)
-
-        returns.reverse()
-        return returns
-
-    def learn(self, episode):
-        returns = self.calculate_returns(episode)
-
+    def learn(self, episode, returns):
         visited = set()
 
         for (state, action, reward), G in zip(episode, returns):
-
-            state_action = (state, action)
-
-            if state_action in visited:
+            if (state, action) in visited:
                 continue
 
-            visited.add(state_action)
+            visited.add((state, action))
 
-            self.returns_sum[state_action] = self.returns_sum.get(state_action, 0) + G
-            self.returns_count[state_action] = self.returns_count.get(state_action, 0) + 1
+            self.returns_sum[(state, action)] = self.returns_sum.get((state, action), 0) + G
+            self.returns_count[(state, action)] = self.returns_count.get((state, action), 0) + 1
 
-            # Q(St, At) = Gt / |Gt|
-            self.Q[state_action] = self.returns_sum[state_action] / self.returns_count[state_action]
-
-
+            self.Q[(state, action)] = self.returns_sum[(state, action)] / self.returns_count[(state, action)]
 
--- monte-carlo/main.py
+++ monte-carlo/main.py
@@ -1,42 +1,33 @@
 import gymnasium as gym
+
 from agent import MCAgent
 
-#create env from the one of Model env't found in Gymnasium
 env = gym.make("Blackjack-v1")
-# creating an agent.
 agent = MCAgent()
 
-#creating initial state. (reset the env and start as new)
 
-no_of_episodes = 10000
+num_of_episodes = 10000
 
-for episode_no in range(no_of_episodes):
+for no_of_ep in range(num_of_episodes):
+
     state, info = env.reset()
 
-    terminated = False
-    truncated = False
-
-    # creating a list to collect an episode
+    terminated, truncated = False, False
     episode = []
 
-    #Generating Episodes
-    while not (terminated):
-
+    while not (terminated or truncated):
         action = agent.choose_action(state)
         next_state, reward, terminated, truncated, _ = env.step(action)
 
         episode.append((state, action, reward))
         state = next_state
 
-    agent.learn(episode)
-
-print(len(agent.Q))
-
-for transition, value in agent.Q.items():
-    print(f"Transition: {transition}, Value: {value}")
+    returns = agent.calculate_returns(episode)
+    agent.learn(episode, returns)
 
 
-
+for state_action, Value in agent.Q.items():
+    print(f"state_action pair: {state_action}, \tValue: {Value}")
 
 
 
